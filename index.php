cae4712f492ffba4c5fe996eb59f6611ec60cb44
Дмитрий Глазвин <sony183@bk.ru>
2016-03-09 14:54:21 +0300

    php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+<?php
+	$a=7;
+	$b=7;
+	$result=$a+$b;
+	$result1=$a-$b;
+	
+	echo "summa= ". $result;
+	echo "summa2= ". $result1;
+ ?>
+	
+</body>
+</html>
