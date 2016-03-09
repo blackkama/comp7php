341b4125183ac12fdcccc552bd1fb52a083e7e73
Дмитрий Глазвин <sony183@bk.ru>
2016-03-09 15:19:48 +0300

    php update

--- index.php
+++ index.php
@@ -3,18 +3,31 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
+	<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
 </head>
 <body>
-
+<div class="front"> 
+<form action="index.php" name="myform" method="post"> 
+       Введите A: <input type="ini_set" name="a" size="10" maxlength="5">
+	   </br>
+       Введите B: <input type="int_set" name="b" size="10" maxlength="5">
+	   <br>
+<input name="Submit" type=submit value="Результат"> 
+</form>
 <?php
-	$a=7;
-	$b=7;
-	$result=$a+$b;
-	$result1=$a-$b;
-	
-	echo "summa= ". $result;
-	echo "summa2= ". $result1;
- ?>
-	
+$a= $_POST['a']; 
+$b=$_POST['b'];
+$result=$a+$b;
+echo "</br>";
+echo "Результат= ".$result;
+
+
+
+
+
+
+?>
+</div>	
 </body>
 </html>
